2a8931604f7b332fe5a22364d40ebd33ff236ab4
ndelucca <ndelucca@educativa.com>
2024-09-11 08:47:51 -0300

    temporarily disable python3.12 sqlite date-datetime adapters - needs db migration to activate

--- timekeeper/database.py
+++ timekeeper/database.py
@@ -1,46 +1,52 @@
 """Database module"""
 
-import logging
-from contextlib import contextmanager
-import sqlite3
 import datetime
+import logging
+import sqlite3
+from contextlib import contextmanager
 from sqlite3 import PARSE_COLNAMES, PARSE_DECLTYPES, Cursor, DatabaseError, connect
 
 
 class TimekeeperDatabaseError(DatabaseError):
     """database module exception"""
 
-def adapt_date_iso(val):
-    """Adapt datetime.date to ISO 8601 date."""
-    return val.isoformat()
 
-def adapt_datetime_iso(val):
-    """Adapt datetime.datetime to timezone-naive ISO 8601 date."""
-    return val.isoformat()
+# def adapt_date_iso(val):
+#     """Adapt datetime.date to ISO 8601 date."""
+#     return val.isoformat()
 
-def adapt_datetime_epoch(val):
-    """Adapt datetime.datetime to Unix timestamp."""
-    return int(val.timestamp())
 
-sqlite3.register_adapter(datetime.date, adapt_date_iso)
-sqlite3.register_adapter(datetime.datetime, adapt_datetime_iso)
-sqlite3.register_adapter(datetime.datetime, adapt_datetime_epoch)
+# def adapt_datetime_iso(val):
+#     """Adapt datetime.datetime to timezone-naive ISO 8601 date."""
+#     return val.isoformat()
 
-def convert_date(val):
-    """Convert ISO 8601 date to datetime.date object."""
-    return datetime.date.fromisoformat(val.decode())
 
-def convert_datetime(val):
-    """Convert ISO 8601 datetime to datetime.datetime object."""
-    return datetime.datetime.fromisoformat(val.decode())
+# def adapt_datetime_epoch(val):
+#     """Adapt datetime.datetime to Unix timestamp."""
+#     return int(val.timestamp())
 
-def convert_timestamp(val):
-    """Convert Unix epoch timestamp to datetime.datetime object."""
-    return datetime.datetime.fromtimestamp(int(val))
+# sqlite3.register_adapter(datetime.date, adapt_date_iso)
+# sqlite3.register_adapter(datetime.datetime, adapt_datetime_iso)
+# sqlite3.register_adapter(datetime.datetime, adapt_datetime_epoch)
 
-sqlite3.register_converter("date", convert_date)
-sqlite3.register_converter("datetime", convert_datetime)
-sqlite3.register_converter("timestamp", convert_timestamp)
+# def convert_date(val):
+#     """Convert ISO 8601 date to datetime.date object."""
+#     return datetime.date.fromisoformat(val.decode())
+
+
+# def convert_datetime(val):
+#     """Convert ISO 8601 datetime to datetime.datetime object."""
+#     return datetime.datetime.fromisoformat(val.decode())
+
+
+# def convert_timestamp(val):
+#     """Convert Unix epoch timestamp to datetime.datetime object."""
+#     return datetime.datetime.fromtimestamp(int(val))
+
+# sqlite3.register_converter("date", convert_date)
+# sqlite3.register_converter("datetime", convert_datetime)
+# sqlite3.register_converter("timestamp", convert_timestamp)
+
 
 @contextmanager
 def open_db(db_name: str) -> Cursor:
